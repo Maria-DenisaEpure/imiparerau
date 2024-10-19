928ac8c921420a09a0c23bb5a673876ea84a9271
BogdanT9 <bogdan.gabriel05@yahoo.com>
2024-10-19 12:04:28 +0300

    NavBar

--- app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "Maps go here"
     }
     val text: LiveData<String> = _text
 }
